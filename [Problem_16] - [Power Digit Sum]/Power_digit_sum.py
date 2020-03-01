2dac673e2363e8637de33de61377a1f734854fe3
dimasyogapratama <dimasburung12@gmail.com>
2020-03-01 14:59:02 +0700

    update

--- [Problem_16] - [Power Digit Sum]/Power_digit_sum.py
+++ [Problem_16] - [Power Digit Sum]/Power_digit_sum.py
@@ -1,14 +1,3 @@
-def power_digit_sum(num, power):
-    power_sum = num ** power
-    print (power_sum)
-    digit_sum = 0
-    for i in str(power_sum):
-        digit_sum += int(i)
-    return digit_sum
-
-print (power_digit_sum(2, 15))
-
-
 def power_digit_sum(num,power):
     power_sum = num**power
     print(power_sum)
